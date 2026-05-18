df20ac848e44a8988edb3eb23e5d93380c9aee46
stephenhowardirl-del <stephen.howardirl@gmail.com>
2026-05-18 22:56:33 +0100

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,15 +3,16 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBBsfvbhpMOLQhQt-5CgazqLCqbkpYZBFY",
-  authDomain: "gigboard-fde2d.firebaseapp.com",
-  projectId: "gigboard-fde2d",
-  storageBucket: "gigboard-fde2d.firebasestorage.app",
-  messagingSenderId: "177652436110",
-  appId: "1:177652436110:web:a98ada1f3e247673f04ddf"
+  apiKey:            process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:        process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId:         process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket:     process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
