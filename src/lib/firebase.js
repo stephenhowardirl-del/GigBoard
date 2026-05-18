d54a7f96b06fb4820d12a19c5f06a201d9078045
stephenhowardirl-del <stephen.howardirl@gmail.com>
2026-05-18 14:31:10 +0100

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -15,6 +15,3 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
-
-googleProvider.addScope('https://www.googleapis.com/auth/calendar.events');
-googleProvider.setCustomParameters({ prompt: 'select_account' });
